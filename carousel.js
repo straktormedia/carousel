c1f16d56c3ba26ecc777f223e891ddcc79bb5b95
straktormedia <straktormedia@gmail.com>
2022-11-17 22:22:01 +0200

    first commit

---
+++ carousel.js
@@ -0,0 +1,42 @@
+// Testimonials Carousel
+
+const buttons = document.querySelectorAll("[data-carousel-btn]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const offset = button.dataset.carouselBtn === "next" ? 1 : -1;
+    // Select it this way to make sure it's working even if we have more carousels.
+    // console.log(offset);
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const dots = document.querySelector("[data-pagination]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    const activeDot = dots.querySelector("[data-active]");
+
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    let newIndexDot = [...dots.children].indexOf(activeDot) + offset;
+
+    console.log(activeDot);
+
+    // Make sure it is working OK for first and last.
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    if (newIndexDot < 0) newIndexDot = dots.children.length - 1;
+    if (newIndexDot >= dots.children.length) newIndexDot = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+
+    dots.children[newIndex].dataset.active = true;
+    delete activeDot.dataset.active;
+
+    dots.children[newIndex].classList.add(
+      "testimonials__pagination__dot--active"
+    );
+    activeDot.classList.remove("testimonials__pagination__dot--active");
+  });
+});
